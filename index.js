cc1df5cc09ef2d5e5463eeeb6907450ee8547ce9
Chotipat Top <top_classic@hotmail.com>
2018-08-27 17:04:50 +0700

    simple can't call func async

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 console.log('before')
-setTimeout(() => { // setTimeout if async func
-  console.log('wait database 2 sec')
-}, 2000)
+const user = getUser(1)
+console.log('user: ', user) // can't return id because wait 2 sec for return
 console.log('after')
-// test non blocking program
+
+function getUser(id){
+  setTimeout(() => { // setTimeout if async func
+    console.log('wait database 2 sec')
+    return { id: id, name: 'top' }
+  }, 2000)
+  return 1
+}
